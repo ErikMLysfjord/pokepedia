16039c5296afec02f98a35b1c90d5da331842957
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2023-09-18 12:33:44 +0200

    test: add snapshot and click event

--- src/components/__tests__/filterselect.test.jsx
+++ src/components/__tests__/filterselect.test.jsx
@@ -1,19 +1,39 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import FilterSelect from "../filter-select/FilterSelect";
+import { fireEvent } from "@testing-library/dom";
 
 describe("FilterSelect", () => {
   test("should render", () => {
-    render(
+    const { container } = render(
       <FilterSelect
-        options={["noe"]}
+        options={["test", "test2"]}
         handleChange={(e) => {
-          console.log(e);
+          console.log(e.target.value);
         }}
         selected="test"
       />
     );
 
+    expect(container).toMatchSnapshot();
     expect(screen.getByText("Test")).toBeInTheDocument();
+    expect(screen.getByRole("combobox")).toBeInTheDocument();
+    expect(screen.getByRole("combobox")).toHaveValue("test");
+    expect(screen.getAllByRole("option")).toHaveLength(2);
+  });
+
+  test("should change option", () => {
+    render(
+      <FilterSelect
+        options={["test", "test2"]}
+        handleChange={(e) => e.target.value}
+        selected="test"
+      />
+    );
+
+    const select = screen.getByRole("combobox");
+
+    fireEvent.select(select, { target: { value: "test2" } });
+    expect(screen.getByRole("combobox")).toHaveValue("test2");
   });
 });
