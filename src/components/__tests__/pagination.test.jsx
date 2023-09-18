7b5e8b9732a8a4bad4a5977ff120513c8da78bb6
Erik Menkin Lysfjord <erikmly@stud.ntnu.no>
2023-09-18 19:03:09 +0200

    test: add test for pagination

--- src/components/__tests__/pagination.test.jsx
+++ src/components/__tests__/pagination.test.jsx
@@ -2,18 +2,7 @@ import { describe, test, expect } from "vitest";
 import Pagination from "../pagination/Pagination";
 import { render, screen } from "@testing-library/react";
 
-const mockUseNavigate = vi.fn();
-const mockUseLocation = vi.fn();
 const mockOnChange = vi.fn();
-
-vi.mock("react-router-dom", async () => {
-  const actual = await vi.importActual("react-router-dom");
-  return {
-    ...actual,
-    useNavigate: () => mockUseNavigate,
-    useLocation: () => mockUseLocation,
-  };
-});
 
 describe("Pagination", () => {
   test("should render", () => {
@@ -21,30 +10,54 @@ describe("Pagination", () => {
       <Pagination
         count={10}
         currentIndex={0}
-        onChange={(index) => mockOnChange(index + 1)}
+        onChange={(index) => {
+          mockOnChange(index + 1);
+        }}
       />
     );
 
     expect(container).toMatchSnapshot();
-    /* expect(screen.getByText("Previous")).toBeInTheDocument();
-    expect(screen.getByText("Next")).toBeInTheDocument(); */
-  });
-
-  test("should navigate to previous page", () => {
-    render(<Pagination />);
-
-    /* screen.getByText("Previous").click();
-
-    expect(mockUseNavigate).toHaveBeenCalledWith("/?page=1");
-    expect(mockUseNavigate).toHaveBeenCalledTimes(1); */
+    expect(screen.getByAltText("Previous")).toBeInTheDocument();
+    expect(screen.getByAltText("Next")).toBeInTheDocument();
+    expect(screen.getByText("1")).toBeInTheDocument();
   });
 
   test("should navigate to next page", () => {
-    render(<Pagination />);
+    render(
+      <Pagination
+        count={10}
+        currentIndex={0}
+        onChange={(index) => mockOnChange(index + 1)}
+      />
+    );
 
-    /* screen.getByText("Next").click();
+    const [previousButton, nextButton] = screen.getAllByRole("button");
 
-    expect(mockUseNavigate).toHaveBeenCalledWith("/?page=2");
-    expect(mockUseNavigate).toHaveBeenCalledTimes(1); */
+    expect(previousButton).toBeDisabled();
+
+    previousButton.click();
+    expect(mockOnChange).toHaveBeenCalledTimes(0);
+
+    nextButton.click();
+    expect(mockOnChange).toHaveBeenCalledOnce();
+    expect(mockOnChange).toHaveBeenCalledWith(2);
+  });
+
+  test("should navigate to previous page", () => {
+    render(
+      <Pagination
+        count={10}
+        currentIndex={1}
+        onChange={(index) => mockOnChange(index + 1)}
+      />
+    );
+
+    mockOnChange.mockClear();
+
+    const [previousButton, nextButton] = screen.getAllByRole("button");
+
+    previousButton.click();
+    expect(mockOnChange).toHaveBeenCalledOnce();
+    expect(mockOnChange).toHaveBeenCalledWith(1);
   });
 });
